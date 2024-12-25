1075b4cc91586c1b5980a461891672fe22aa8031
Lightmker <Lightmker7@gmail.com>
2024-12-25 11:21:52 +0200

    Update set.js

--- set.js
+++ set.js
@@ -5,7 +5,7 @@ const fs = require('fs'),
       convertToBool = (text, fault = 'true') => text === fault;
 
 global.caption = "*©²⁰²⁵ ᴺᵗᵃⁿᵈᵒ ⱽ¹💙*"; // Input Yours custom...(Maintain font for Flow)
-global.api = "https://api.giftedtech.my.id"; // DO NOT Change this...
+global.api = "https://api.giftedtech.my.id/api"; // DO NOT Change this...
 global.session = "https://pair1.giftedtech.my.id"; // DO NOT Change this....
 global.footer = "*ᴾᵒʷᵉʳᵉᵈ ᵇʸ ᴺᵗᵃⁿᵈᵒ ᵀᵉᶜʰ*"; // Input Yours custom...(Maintain font for Flow)
 global.myName = process.env.API_KEY;
@@ -28,22 +28,22 @@ OWNER_NAME: process.env.OWNER_NAME || "ᴺᵗᵃⁿᵈᵒ ᵀᵉᶜʰ", // Input
 PACK_AUTHOR: process.env.PACK_AUTHOR || "ᴺᵗᵃⁿᵈᵒ ᵀᵉᶜʰ", // Added // Input Yours custom...
 PACK_NAME: process.env.PACK_NAME || "©²⁰²⁵ ᴺᵗᵃⁿᵈᵒ ⱽ¹💙", // Added // Input Yours custom...
 PREFIX: process.env.PREFIX || ".",
-API_KEY: process.env.API_KEY || "gifted-md", // Replace with your unlimited api key
+API_KEY: process.env.API_KEY || "©²⁰²⁵ ᴺᵗᵃⁿᵈᵒ ⱽ¹💙", // Replace with your unlimited api key
 // ANTIDELETE: process.env.ANTIDELETE || "false", // Still under development
 ANTILINK: process.env.ANTILINK || "false", //  Enter true to kick automatically or delete to delete without kicking or warn to warn before kicking
 ANTICALL: process.env.ANTICALL || "false",
 ANTIBAD: process.env.ANTIBAD || "false",
 BAD_WORDS: process.env.BAD_WORDS || "fuck, pussy, anus, idiot", // Add Yours Separated by Comma(will be deleted if ANTIBAD is set to true)
 ANTICALL_MSG: process.env.ANTICALL_MSG || "*_📞 Auto Call Reject Mode Activated by Gifted-Md. 📵 No Calls Allowed Dude!_*",
-AUTO_REACT: process.env.AUTO_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "©²⁰²⁵ ᴺᵗᵃⁿᵈᵒ ⱽ¹💙", // Input Yours custom...(Maintain font for Flow)
-BOT_PIC: process.env.BOT_PIC || "https://files.giftedtech.my.id/file/gifted-md.jpg", // You can Replace with yours...
-AUTO_AUDIO: process.env.AUTO_AUDIO || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
+BOT_NAME: process.env.BOT_NAME || "ᴺᵗᵃⁿᵈᵒ ⱽ¹💙", // Input Yours custom...(Maintain font for Flow)
+BOT_PIC: process.env.BOT_PIC || "https://i.ibb.co/XFqmWhs/file-5885.jpg", // You can Replace with yours...
+AUTO_AUDIO: process.env.AUTO_AUDIO || "true",
 AUTO_BIO: process.env.AUTO_BIO || "false",
 AUTO_BIO_QUOTE: process.env.AUTO_BIO_QUOTE || "Gifted creates things that creates other things",
-CHAT_BOT: process.env.CHAT_BOT || "false", // Put value to true to enablle for all chats only or inbox to ebanle in pm chats only or groups to enable in groups only else false
-WELCOME: process.env.WELCOME || "false",
-GOODBYE: process.env.GOODBYE || "false",    
+CHAT_BOT: process.env.CHAT_BOT || "true", // Put value to true to enablle for all chats only or inbox to ebanle in pm chats only or groups to enable in groups only else false
+WELCOME: process.env.WELCOME || "true",
+GOODBYE: process.env.GOODBYE || "true",    
 AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "false", // Enter value to true for blueticking all messages, or commands for blueticking only commands else false
 AUTO_BLOCK: process.env.AUTO_BLOCK || "212,79", // Add Multiple Country Codes Separated by Comma...
 PRESENCE: process.env.PRESENCE || "online", // Choose one: typing, recording, online, null
